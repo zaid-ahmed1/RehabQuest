a39019bfef6754ae0f6ac0e765b90811cf233d20
Zaid <zaid.ahmed@ucalgary.ca>
2024-07-28 01:18:51 -0600

    initial commit.  setup and gitingore

---
+++ Assets/Scripts/WriteToS3.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.S3.Model;
+using UnityEngine;
+public class WriteToS3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
