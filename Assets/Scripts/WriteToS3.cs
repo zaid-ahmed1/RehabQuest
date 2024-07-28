3afab465f5e6010b9e1085a14a38aa210073f3d2
Zaid <zaid.ahmed@ucalgary.ca>
2024-07-28 01:51:45 -0600

    prelim function

--- Assets/Scripts/WriteToS3.cs
+++ Assets/Scripts/WriteToS3.cs
@@ -5,13 +5,26 @@ using System.Threading.Tasks;
 using Amazon.S3;
 using Amazon.S3.Model;
 using UnityEngine;
+usign Fi
 public class WriteToS3 : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    public IAmazonS3 s3Client;
+    
+    string jsonPath = "secrets.json";
     
     void Start()
     {
+        // debug log the current path
+        Debug.Log(Application.persistentDataPath);
+        
+        
+        AmazonS3Config s3Config = new AmazonS3Config();
+        s3Config.RegionEndpoint = Amazon.RegionEndpoint.USEast2;
+        
+        // Building S3 client with Access Key and Secret Access Key
+        s3Client = new AmazonS3Client("AKIAUUCCXU7QHL2TK37P", "lXu3H7vuoi9krlVQ8LpaPDJCmEXvnhOh56t1U3tT", s3Config);
+        
         
     }
 
