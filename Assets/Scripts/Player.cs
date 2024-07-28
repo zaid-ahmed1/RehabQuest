fb7929ae2aee6a1bbbd10e52e040183d6a146a79
Zaid <zaid.ahmed@ucalgary.ca>
2024-07-28 07:23:20 -0600

    fixed bug with s3

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,19 +16,24 @@ public class Player : MonoBehaviour
     public string parent = "Parent";
 
     public WriteToS3 wts3;
-    
+
     void OnApplicationQuit()
     {
         // Save data to file
+        // print out all the data
+        Debug.Log("Task 1 Complete: " + task1Complete);
+        Debug.Log("Time to complete task 1: " + timeToCompleteTask1);
+        Debug.Log("Name: " + name);
         totalTime  = Timer.getTime();
         string data = JsonUtility.ToJson(this);
         string timestamp = System.DateTime.Now.ToString("yyyyMMddHHmmss");
-        
+
         // Save data to local file
-        System.IO.File.WriteAllText(Application.persistentDataPath  + "/"  + parent + "/" + name +"/" + timestamp +  ".json", data);
+        System.IO.File.WriteAllText("C:\\Users\\zaida\\Desktop\\HealthHacks" + name + timestamp +  ".json", data);
         // Save data to S3
-        wts3.UploadFile(Application.persistentDataPath + "/"  + parent + "/" + name  + timestamp +  ".json",
-            Application.persistentDataPath  + "/"  + parent + "/" + name +"/" + timestamp +  ".json");
+        wts3.UploadFile( 
+            Application.persistentDataPath  + "/"  + parent + "/" + name +"/" + timestamp +  ".json",
+        "C:\\Users\\zaida\\Desktop\\HealthHacks" + name  + timestamp +  ".json");
         // Debug.Log(Application.persistentDataPath + "/player" + name + timestamp + ".json");
 
     }
